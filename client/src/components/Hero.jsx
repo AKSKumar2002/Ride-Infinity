ef7c9032c3fcbeffc6b634fdccb32aac29a7092f
Mr. AKS <aksnetlink@gmail.com>
2025-07-29 11:33:52 +0530

    s

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -34,14 +34,14 @@ const Hero = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
-      className="py-20 px-4 flex flex-col items-center justify-center gap-10 bg-light text-center overflow-hidden"
+      className="py-20 px-4 flex flex-col items-center justify-center gap-10 bg-light text-center overflow-x-hidden"
     >
       {/* Heading */}
       <motion.h1
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8, delay: 0.2 }}
-        className="text-3xl md:text-5xl font-semibold flex items-center justify-center gap-2 flex-wrap"
+        className="text-3xl md:text-5xl font-semibold flex items-center justify-center gap-2"
       >
         Need a
         <span className="relative w-[100px] h-[50px] md:w-[130px] md:h-[60px] overflow-hidden inline-block align-middle">
@@ -62,48 +62,42 @@ const Hero = () => {
         ?
       </motion.h1>
 
-      {/* 3 Bikes Layout */}
+      {/* 3 Bikes Layout: Always in Row */}
       <motion.div
         initial={{ y: 100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8, delay: 0.6 }}
-        className="flex flex-wrap items-end justify-center gap-6 md:gap-10 w-full max-w-[95vw] overflow-hidden"
+        className="flex items-end justify-center gap-3 sm:gap-6 md:gap-10 w-full max-w-[95vw] overflow-x-auto"
       >
         {/* Left Bike */}
-        <div className="flex justify-center items-end w-full sm:w-1/3">
-          <motion.img
-            src={assets.bmw}
-            alt="Left bike"
-            className="h-36 sm:h-52 md:h-60 lg:h-64 scale-x-[-1] object-contain"
-            initial={{ x: -100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ delay: 0.5 }}
-          />
-        </div>
+        <motion.img
+          src={assets.bmw}
+          alt="Left bike"
+          className="h-40 sm:h-48 md:h-60 lg:h-64 scale-x-[-1] object-contain"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.5 }}
+        />
 
         {/* Center Bike */}
-        <div className="flex justify-center items-end w-full sm:w-1/3">
-          <motion.img
-            src={assets.bullet}
-            alt="Center bike"
-            className="h-36 sm:h-52 md:h-60 lg:h-64 object-contain"
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.7 }}
-          />
-        </div>
+        <motion.img
+          src={assets.bullet}
+          alt="Center bike"
+          className="h-40 sm:h-48 md:h-60 lg:h-64 object-contain"
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.7 }}
+        />
 
         {/* Right Bike */}
-        <div className="flex justify-center items-end w-full sm:w-1/3">
-          <motion.img
-            src={assets.kawasaki}
-            alt="Right bike"
-            className="h-36 sm:h-52 md:h-60 lg:h-64 object-contain"
-            initial={{ x: 100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ delay: 0.5 }}
-          />
-        </div>
+        <motion.img
+          src={assets.kawasaki}
+          alt="Right bike"
+          className="h-40 sm:h-48 md:h-60 lg:h-64 object-contain"
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.5 }}
+        />
       </motion.div>
     </motion.div>
   );
