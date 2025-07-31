8dd639cfc341386c472da711c416c5bff8f52324
Mr. AKS <aksnetlink@gmail.com>
2025-07-31 15:28:34 +0530

    F

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -18,7 +18,7 @@ const Banner = () => {
             <motion.button 
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
-            className='px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer'>List your car</motion.button>
+            className='px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer'>List your bike</motion.button>
         </div>
 
         <motion.img 
